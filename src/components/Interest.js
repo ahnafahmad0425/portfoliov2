00ae7e1ae4bab7151baab98c83a9267c78047a91
Ahnaf Ahmad <ahnafahmad0425@gmail.com>
2024-05-29 19:26:55 -0500

    Fixed consistency in interests

--- src/components/Interest.js
+++ src/components/Interest.js
@@ -6,9 +6,9 @@ const Interest = () => {
     <section className="interests">
       <h2>Current Interests</h2>
       <ul>
-        <li>iOS App Development</li>
+        <li>iOS Application Development</li>
         <li>AI / Machine learning & Data Science</li>
-        <li>Full stack web development</li>
+        <li>Full Stack Web Development</li>
       </ul>
     </section>
   );
